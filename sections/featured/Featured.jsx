9eada953b2891d96ea3c168a9661630a5e067f31
Nasyx Nadeem <sdarnadeem@gmail.com>
2022-05-28 17:09:38 +0530

    add feature animation

--- sections/featured/Featured.jsx
+++ sections/featured/Featured.jsx
@@ -1,6 +1,8 @@
 import React, { useRef } from "react";
 import c from "./Featured.module.css";
 
+import { motion } from "framer-motion";
+
 import { useEffect } from "react";
 import AOS from "aos";
 
@@ -13,20 +15,28 @@ function Featured(props) {
 
   return (
     <section className={c.featuredSection} data-scroll-section>
-      <div className={c.featuredRowLayout}>
-        <div></div>
-
+      <motion.div
+        // whileInView={{ scale: 1.2 }}
+        // // animate={{ x: 0 }}
+        // transition={{ duration: 1.5, type: "tween", delay: 0.2 }}
+        // key="hellowddselhfsdfh"
+        className={c.featuredRowLayout}
+      >
         <img src="/images/locomotive/couples2.jpg" />
         {/* </div> */}
-      </div>
-      <div className={c.featuredColumnLayout}>
-        <div></div>
-        <div className={c.imageContainer}>
+      </motion.div>
+      <motion.div className={c.featuredColumnLayout}>
+        <motion.div
+          initial={{ x: 35 }}
+          whileInView={{ x: 0 }}
+          // animate={{ x: 0 }}
+          transition={{ duration: 1.5, type: "tween", delay: 0.2 }}
+          key="hellowddselhfsdfkh"
+          className={c.imageContainer}
+        >
           <img src="/images/locomotive/couples3.jpg" />
-        </div>
-
-        <div></div>
-      </div>
+        </motion.div>
+      </motion.div>
     </section>
   );
 }
